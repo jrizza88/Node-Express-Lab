5d76e8fe6bb4c13e6f85b310769b70507d20503c
jrizza88 <jetorres88@gmail.com>
2019-03-01 22:23:16 -0500

    successfully created a new post

--- data/postRoutes.js
+++ data/postRoutes.js
@@ -7,16 +7,49 @@ const router = express.Router();
 router.get('/', async (req, res) => {
     // use try instead of then
     try {
-        console.log('query', req.query)
-    // define a variable that points to the db above with the 
-    // find method and use request query
+        // console.log('query', req.query)
         const posts = await db.find(req.query)
         res.status(200).json(posts);
-    } catch {
+    } catch (error) {
         console.log(error)
-        res.status(404).json({error: "The posts information could not be retrieved."})
+        res.status(500).json({error: "The posts information could not be retrieved."})
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await db.findById(req.params.id);
+        console.log('find by id', req.params.id)
+     
+        if (post){
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({message: "The post with the specified ID does not exist."})
+            return;
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error: "The post information could not be retrieved."})
+    }
+});
+
+router.post('/', async (req, res) => {
+ const {title, contents} = req.body
+    if (!title || !contents) {
+        res.status(404).json({errorMessage: "Please provide title and contents for the post."})
+        return;
     }
 
-});
+    try {
+        const postBody = req.body;
+        const post = await db.insert(postBody)
+        console.log(postBody)
+        if (postBody) {
+            res.status(201).json(postBody)
+        } 
+    } catch (error) {
+        res.status(500).json({error: "There was an error while saving the post to the database" })
+    }
+})
 
 module.exports = router;
